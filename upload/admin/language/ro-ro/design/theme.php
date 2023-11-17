8d17a24e8106bcbe3c302456dbce628ea7a4bf6a
condor2 <condor20_05@yahoo.it>
2023-11-17 19:25:51 +0200

    Update theme.php

--- upload/admin/language/ro-ro/design/theme.php
+++ upload/admin/language/ro-ro/design/theme.php
@@ -6,7 +6,7 @@ $_['heading_title']     = 'Editor Temă';
 $_['text_success']      = 'Succes: Ai modificat temele!';
 $_['text_edit']         = 'Editare Temă';
 $_['text_store']        = 'Alege magazinul';
-$_['text_template']     = 'Alege o temă';
+$_['text_template']     = 'Alege un șablon';
 $_['text_default']      = 'Implicit';
 $_['text_history']      = 'Istoric Temă';
 $_['text_twig']         = 'Editorul de temă folosește limbajul șablonului Twig. Poți citi despre <a href="https://twig.symfony.com/doc/" target="_blank" class="alert-link">sintaxa Twig aici</a>.';
