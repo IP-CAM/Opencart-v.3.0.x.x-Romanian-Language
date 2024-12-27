34dd80fd3b2d1da1148c9b290b4f712ab8f9de3a
condor2 <condor20_05@yahoo.it>
2024-12-27 11:52:36 +0200

    ECB & Fixer translations updated

--- upload/admin/language/ro-ro/extension/currency/ecb.php
+++ upload/admin/language/ro-ro/extension/currency/ecb.php
@@ -1,15 +1,26 @@
 <?php
 // Heading
-$_['heading_title']    = 'Convertor valutar Banca Centrală Europeană';
+$_['heading_title']    = 'Convertor valutar - ECB';
 
 // Text
 $_['text_extension']   = 'Extensii';
-$_['text_success']     = 'Succes: Ai modificat convertorul valutar BCE!';
-$_['text_edit']        = 'Editare Banca Centrală Europeană';
-$_['text_support']     = 'Această extensie necesită ca moneda EUR să fie disponibilă în lista de valute.';
+$_['text_success']     = 'Succes: Ai modificat convertorul valutar ECB!';
+$_['text_edit']        = 'Editare convertor valutar ECB';
+$_['text_edit']       .= '<br><br>';
+$_['text_edit']       .= 'Această extensie necesită ca moneda EUR să fie disponibilă în lista de valute.';
+$_['text_edit']       .= '<br><br>';
+$_['text_edit']       .= 'Când este activat, poate fi selectat ca motor de actualizare a cursului valutar în <b><a href="%2">Sistem &gt; Setări</b></a> iar ratele de schimb valutar pot fi actualizate prin <b><a href="%1">Sistem &gt; Localizare &gt; Valute</a></b>.';
 
 // Entry
+$_['entry_ip']         = 'Adresă IP server';
+$_['entry_cron']       = 'Comandă CRON';
 $_['entry_status']     = 'Status';
 
+// Help
+$_['help_ip']          = 'Adresa IP opțională pentru serverul care rulează o operațiune CRON pentru actualizarea cursurilor valutare.';
+$_['help_cron']        = 'Utilizează această comandă atunci când configurezi o operațiune CRON pe serverul tău.';
+
 // Error
-$_['error_permission'] = 'Avertizare: Nu ai permisiunea de a modifica convertorul valutar BCE!';
+$_['error_permission'] = 'Avertizare: Nu ai permisiunea de a modifica convertorul valutar ECB!';
+$_['error_euro']       = 'Această extensie necesită ca Euro să fie disponibil ca monedă.!';
+$_['error_ip']         = 'Adresă IP invalidă!';
--- upload/admin/language/ro-ro/extension/currency/fixer.php
+++ upload/admin/language/ro-ro/extension/currency/fixer.php
@@ -1,18 +1,27 @@
 <?php
 // Heading
-$_['heading_title']    = 'Fixer';
+$_['heading_title']    = 'Convertor valutar - Fixer';
 
 // Text
 $_['text_extension']   = 'Extensii';
 $_['text_success']     = 'Succes: Ai modificat convertorul valutar Fixer!';
-$_['text_edit']        = 'Editare Fixer';
-$_['text_signup']      = 'Fixer.io este un serviciu de conversie valutară care se <a href="https://fixer.io/" target="_blank" class="alert-link">înregistrează aici</a>.';
-$_['text_support']     = 'Această extensie necesită ca moneda EUR să fie disponibilă în lista de valute.';
+$_['text_edit']        = 'Editare convertor valutar Fixer';
+$_['text_edit']       .= '<br><br>';
+$_['text_edit']       .= 'Fixer.io este un serviciu de conversie valutară. Înainte de a-l folosi pentru prima dată, trebuie să te <b><a href="https://fixer.io/" target="_blank">înregistrezi aici</a></b> pentru a obține o cheie de acces API.';
+$_['text_edit']       .= '<br><br>';
+$_['text_edit']       .= 'Când este activat, poate fi selectat ca motor de actualizare a cursului valutar în <b><a href="%2">Sistem &gt; Setări</b></a> iar ratele de schimb valutar pot fi actualizate prin <b><a href="%1">Sistem &gt; Localizare &gt; Valute</a></b>.';
 
 // Entry
 $_['entry_api']        = 'Cheie de acces API';
+$_['entry_ip']         = 'Adresă IP server';
+$_['entry_cron']       = 'Comandă CRON';
 $_['entry_status']     = 'Status';
+
+// Help
+$_['help_ip']          = 'Adresa IP opțională pentru serverul care rulează o operațiune CRON pentru actualizarea cursurilor valutare.';
+$_['help_cron']        = 'Utilizează această comandă atunci când configurezi o operațiune CRON pe serverul tău.';
 
 // Error
 $_['error_permission'] = 'Avertizare: Nu ai permisiunea de a modifica convertorul valutar Fixer!';
+$_['error_ip']         = 'Adresă IP invalidă!';
 $_['error_api']        = 'Cheia de acces API este obligatorie!';
